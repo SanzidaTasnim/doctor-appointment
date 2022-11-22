2eb3046c7d72a13c61e1a3ef435dd619a6ac1536
SanzidaTasnim <tasnim5sanzida@gmail.com>
2022-11-22 12:22:26 +0600

    about page comitted

--- src/components/Shared/Nav/Nav.js
+++ src/components/Shared/Nav/Nav.js
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { createUser } from '../../../App';
+import {useNavigate, useLocation} from 'react-router';
 import './Nav.css';
 
 const Nav = ({textLight }) => {
    const [loggedInUser , setLoggedInUser] = useContext(createUser);
    const navColor = textLight ? 'text-light' : 'text-muted';
+   const navigate = useNavigate();
+   const location = useLocation();
+   const handleLogin = () => {
+      navigate('/dashboard');
+      location.state?.from && navigate(location.state.from);
+   }
    
    return (
       <div>
@@ -25,12 +32,9 @@ const Nav = ({textLight }) => {
                      <Link className={`nav-link ${navColor}`} to="/dashboard">Dashboard</Link>
                   </li> 
                   <li className="nav-item">
-                     <button onClick={() => setLoggedInUser({})}>
-                        {
-                           loggedInUser.email ? 'LogOut' : 'Login'
-                        }
-                     </button>
-                  <p>{loggedInUser.email}</p>
+                     {
+                        loggedInUser.email ? <button onClick={() => setLoggedInUser({})} className='btn btn-secondary'>LogOut</button> : <button onClick={handleLogin} className='btn btn-secondary'>Login</button>
+                     }
                   </li> 
                   
                </ul>
